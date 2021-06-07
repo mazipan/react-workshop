d80af5c77ff8ebb36a87ce89c1e47c1dbb4216d6
miqdadfwz <miqdad.fawwaz@tokopedia.com>
2021-06-07 17:23:19 +0700

    improvement: add apollo client in server

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'import/order': 0,
+    'import/no-dynamic-require': 0,
+    'arrow-body-style': 0,
+    'no-nested-ternary': 0,
+    'react/jsx-filename-extension': 0,
+    'no-underscore-dangle': 0,
+    'react/no-danger': 0,
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,56 +1,82 @@
+import 'isomorphic-unfetch';
 import App from './App';
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
-import express from 'express';
 import { renderToString } from 'react-dom/server';
+import express from 'express';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { getDataFromTree } from '@apollo/client/react/ssr';
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const razzleAssets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const cssLinksFromAssets = (assets, entrypoint) => {
-  return assets[entrypoint] ? assets[entrypoint].css ?
-  assets[entrypoint].css.map(asset=>
-    `<link rel="stylesheet" href="${asset}">`
-  ).join('') : '' : '';
+  return assets[entrypoint]
+    ? assets[entrypoint].css
+      ? assets[entrypoint].css.map((asset) => <link rel="stylesheet" href={asset} />)
+      : ''
+    : '';
 };
 
-const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
-  return assets[entrypoint] ? assets[entrypoint].js ?
-  assets[entrypoint].js.map(asset=>
-    `<script src="${asset}"${extra}></script>`
-  ).join('') : '' : '';
+const jsScriptTagsFromAssets = (assets, entrypoint) => {
+  return assets[entrypoint]
+    ? assets[entrypoint].js
+      ? assets[entrypoint].js.map((asset) => <script src={`${asset}`} defer crossOrigin />)
+      : ''
+    : '';
 };
 
 const server = express();
+
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
+    const apolloClient = new ApolloClient({
+      ssrMode: true,
+      cache: new InMemoryCache(),
+      link: createHttpLink({
+        uri: 'https://asia-southeast2-minitokopedia.cloudfunctions.net/graphql',
+      }),
+    });
+
     const context = {};
-    const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
+    const markup = (
+      <ApolloProvider client={apolloClient}>
+        <StaticRouter context={context} location={req.url}>
+          <App />
+        </StaticRouter>
+      </ApolloProvider>
     );
 
     if (context.url) {
       res.redirect(context.url);
     } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${cssLinksFromAssets(assets, 'client')}
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-        ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
-    </body>
-</html>`
-      );
+      getDataFromTree(markup).then((content) => {
+        const initialState = apolloClient.extract();
+
+        const html = renderToString(
+          <html lang="en">
+            <head>
+              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+              <meta charset="utf-8" />
+              <title>Welcome to Razzle</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              {cssLinksFromAssets(razzleAssets, 'client')}
+            </head>
+            <body>
+              <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.__APOLLO_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')};`,
+                }}
+              />
+              {jsScriptTagsFromAssets(razzleAssets, 'client')}
+            </body>
+          </html>,
+        );
+
+        res.status(200).header('Content-Type', 'text/html').send(`<!doctype html>\n${html}`);
+      });
     }
   });
 
