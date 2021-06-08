09b31f4709adf68b4e3084c2e474a1c60f7cbdc2
miqdadfwz <miqdad.fawwaz@tokopedia.com>
2021-06-08 10:33:11 +0700

    feat: setup apollo for client

--- src/client.js
+++ src/client.js
@@ -2,12 +2,20 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+  uri: 'https://asia-southeast2-minitokopedia.cloudfunctions.net/graphql',
+});
 
 hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
 
 if (module.hot) {
